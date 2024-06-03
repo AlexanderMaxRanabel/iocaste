e3f6c5b1015129ad5c8d8a2a3126cbbc778351b6
choroalp <choroalp@skiff.com>
2024-06-03 10:13:43 +0300

    Updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Iocaste
 
-Iocaste is an archiving tool developed for Gemini protocol in rust.
+Iocaste is an archiving tool developed for Gemini protocol in rust. Part of Helios project.
+Insipired from an earlier project of mine for http, Archeal.
+
+# OS Support
+- Windows: Currently unsupported.
+- Linux: Supported.
+- MacOS: Currently unsupported.
--- src/depth_gempage.rs
+++ src/depth_gempage.rs
@@ -1,15 +1,15 @@
 use crate::domain_utils;
 use crate::fetch_gempage;
 
-use std::fs;
-use std::io::Write;
+use std::{fs, io::Write};
+
+use colored::*;
 
 pub async fn depth_based(gem_body: String, current_path: String, url: String) -> anyhow::Result<()> {
     let mut anchor_links: Vec<String> = Vec::new();
     anchor_links = domain_utils::extract_links(anchor_links, gem_body.clone(), url.clone()).await?;
 
     for sublink in anchor_links {
-        println!("{}", sublink.clone());
         let sublink_gem_body = fetch_gempage::mk_req(sublink.clone()).await?;
 
         let sublink_domain = domain_utils::get_proper_domain(url.clone()).await?;
@@ -19,8 +19,6 @@ pub async fn depth_based(gem_body: String, current_path: String, url: String) ->
             .chars()
             .filter(|&c| c.is_ascii_alphanumeric() || c == '-' || c == '.')
             .collect::<String>();
-
-        //let sublink_path = sublink_parsed_url.path().trim_start_matches('/');
 
         let sublink_path_with_dashes = sublink_path.replace("/", "-");
 
@@ -32,6 +30,7 @@ pub async fn depth_based(gem_body: String, current_path: String, url: String) ->
 
         let sublink_gem = fs::File::create(sublink_gem_path.clone());
 
+        println!("{}: Writing: {} to {}", "LOG".yellow().bold(), sublink.clone(), sublink_gem_path.clone());
 
         sublink_gem
             .expect("Cannot write to this file")
